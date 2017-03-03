aa6c298b957746b74dbaa25ed6666eec1661ef23
Maxence Maireaux <maxence@realstorm.fr>
2017-03-03 16:58:49 +0100

    Fix AWS

--- terraform/aws/ressource_glusterfs.tf
+++ terraform/aws/ressource_glusterfs.tf
@@ -6,17 +6,6 @@ resource "aws_instance" "swarm_glusterfs" {
   key_name = "${var.aws_key_name}"
   security_groups = ["${aws_security_group.swarm.name}"]
 
-  connection {
-    user     = "${var.aws_ami_user}"
-    private_key = "${file("${var.aws_ssh_key}")}"
-    agent = false
-  }
-
-  provisioner "file" {
-    source = "swarm.token"
-    destination = "/tmp/node.token"
-  }
-
   provisioner "local-exec" {
     command = "sleep 30 && ansible-playbook -i '${self.public_ip},' --private-key ${var.aws_ssh_key} '../../ansible/swarm_glusterfs.ansible.yaml' -T 300"
   }
