34079b57f891f7d85183a952d756bdb7a41304d6
Maxence Maireaux <maxence@realstorm.fr>
2017-04-18 16:11:16 +0200

    [CLI] Feature : Update configuration for CLI

--- cli/configuration.go
+++ cli/configuration.go
@@ -3,16 +3,17 @@ package main
 type Configuration struct {
 	Header struct {
 		Version string `json:"version"`
+		Environment string `json:"environment"`
 	} `json:"header"`
 	Provider struct {
 		Aws struct {
 			AccessKey    string `json:"access_key" env:"TF_VAR_aws_access_key"`
 			SecretKey    string `json:"secret_key" env:"TF_VAR_aws_secret_key"`
 			Ami          string `json:"ami" env:"TF_VAR_aws_ami"`
-			KeyName      string `json:"key_name" env:"TF_VAR_aws_key_name"`
+			KeyName      string `json:"ssh_key" env:"TF_VAR_aws_ssh_key"`
 			Region       string `json:"region" env:"TF_VAR_aws_region"`
 			SwarmNodes   string `json:"swarm_nodes" env:"TF_VAR_swarm_nodes"`
-			SwarmManager string `json:"swarm_maanger" env:"TF_VAR_swarm_manager"`
+			SwarmManager string `json:"swarm_manager" env:"TF_VAR_swarm_manager"`
 			TypeManager  string `json:"type_manager" env:"TF_VAR_aws_type_manager"`
 			TypeNode     string `json:"type_node" env:"TF_VAR_aws_type_node"`
 		} `json:"aws"`
