58130e121d425193c607b5e6cb46203b9cbf85ec
Maxence Maireaux <maxence@realstorm.fr>
2017-04-25 21:36:29 +0200

    [ENGINE] Remove debug in ansible

--- engine/bin/create.sh
+++ engine/bin/create.sh
@@ -27,7 +27,7 @@ if [ "$1" == "aws" ]; then
     # Launch Ansible
     platform=$(uname)
     if [[ $platform == 'Linux' ]]; then
-        ansible-playbook ./../../ansible/init.ansible.yaml -i terraform-inventory-linux --private-key ../../data/sweady --user=ubuntu -e "efs_url=$(terraform output  mount_target_master_public_subnets_dns_name)" -vvvv
+        ansible-playbook ./../../ansible/init.ansible.yaml -i terraform-inventory-linux --private-key ../../data/sweady --user=ubuntu -e "efs_url=$(terraform output  mount_target_master_public_subnets_dns_name)"
     elif [[ $platform == 'Darwin' ]]; then
         ansible-playbook ./../../ansible/init.ansible.yaml -i terraform-inventory-darwin --private-key ../../data/sweady --user=ubuntu -e "efs_url=$(terraform output  mount_target_master_public_subnets_dns_name)"
     fi
